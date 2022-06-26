28866208a03634506dc69d5b67f0ad78f3857323
IDstorage <idstorage1892@gmail.com>
2022-06-27 03:24:47 +0900

    Fix player movement animation

--- project-kata-unity/Assets/Scripts/CharacterComponent.cs
+++ project-kata-unity/Assets/Scripts/CharacterComponent.cs
@@ -16,18 +16,18 @@ public class CharacterComponent : CustomComponent
         public float moveSpeedMultiflier;
     }
 
-    public Vector3 UpdateMovement(Data target, Vector3 camForward, float h, float v)
+    public bool UpdateMovement(Data target, Vector3 camForward, float h, float v, out Vector3 moveDir)
     {
-        var moveDir = camForward * v + Quaternion.AngleAxis(90F, Vector3.up) * camForward * h;
-        moveDir.Normalize();
+        moveDir = camForward * v + Quaternion.AngleAxis(90F, Vector3.up) * camForward * h;
+        //moveDir.Normalize();
         //var moveDir = target.character.transform.forward * v + target.character.transform.right * h;
 
         bool isMoving = Mathf.Abs(h) >= Mathf.Epsilon || Mathf.Abs(v) >= Mathf.Epsilon;
-        if (!isMoving) return Vector3.zero;
+        if (!isMoving) return false;
 
         //target.character.transform.rotation = Quaternion.Slerp(target.character.transform.rotation, quat, Time.deltaTime * 10F);
         //target.character.Move((target.character.transform.forward * target.moveSpeed) * target.moveSpeedMultiflier * Time.deltaTime);
         target.character.Move((moveDir * target.moveSpeed) * target.moveSpeedMultiflier * Time.deltaTime);
-        return moveDir;
+        return true;
     }
 }
--- project-kata-unity/Assets/Scripts/Player.cs
+++ project-kata-unity/Assets/Scripts/Player.cs
@@ -14,12 +14,31 @@ public class Player : CustomObject
     [SerializeField]
     private Transform model;
 
+    [SerializeField]
+    private Animator animator;
+
+    [SerializeField]
+    private float animationFadeMultiplier = 1F;
+
+
     private ThirdPersonComponent thirdPerson = null;
     private CharacterComponent character = null;
+
+    private float hFollower = 0F, vFollower = 0F;
+    private float floatCutoff = 0.005f;
+
+    private float GetSign(float value)
+    {
+        if (Mathf.Abs(value) <= floatCutoff) return 0F;
+        return Mathf.Sign(value);
+    }
+
 
     protected override void Initialize()
     {
         base.Initialize();
+
+        Cursor.lockState = CursorLockMode.Locked;
 
         thirdPerson = GetSharedComponent<ThirdPersonComponent>();
         thirdPerson.InitializeCamera(thirdPersonData);
@@ -32,11 +51,23 @@ public class Player : CustomObject
         thirdPerson.HandleMouseInput(thirdPersonData, Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
         thirdPerson.HandleCameraLook(thirdPersonData);
 
-        var dir = character.UpdateMovement(characterData, thirdPerson.GetForwardVector(thirdPersonData),
-                                                 Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        float h = Input.GetAxis("Horizontal"),
+            v = Input.GetAxis("Vertical");
 
-        if (dir != Vector3.zero)
-            model.rotation = Quaternion.Slerp(model.rotation, Quaternion.LookRotation(dir, model.up), Time.deltaTime * 20F);
+        hFollower += GetSign(h - hFollower) * Time.deltaTime * animationFadeMultiplier;
+        if (Mathf.Abs(hFollower) <= floatCutoff) hFollower = 0F;
+
+        vFollower += GetSign(v - vFollower) * Time.deltaTime * animationFadeMultiplier;
+        if (Mathf.Abs(vFollower) <= floatCutoff) vFollower = 0F;
+
+        bool isMoving = character.UpdateMovement(characterData, thirdPerson.GetForwardVector(thirdPersonData), h, v, out var dir);
+
+        if (isMoving)
+            model.rotation = Quaternion.Slerp(model.rotation, Quaternion.LookRotation(dir.normalized, model.up), Time.deltaTime * 10F);
+
+        animator.SetFloat("VSpeed", Mathf.Clamp01(Mathf.Abs(h) + Mathf.Abs(v)));
+
+        if (Input.GetMouseButtonDown(0)) animator.SetTrigger("DefaultAttack");
 
         Debug.DrawRay(transform.position, thirdPerson.GetForwardVector(thirdPersonData) * 5F, Color.red);
     }
