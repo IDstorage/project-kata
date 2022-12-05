9db2b7dfb8afb11edcc020831f9576353ecc4fe7
IDstorage <idstorage1892@gmail.com>
2022-12-05 15:39:57 +0900

    Add PatternSequence (execute patterns by linked list)

--- project-kata-unity/Assets/Scripts/Data/Pattern/Pattern.cs
+++ project-kata-unity/Assets/Scripts/Data/Pattern/Pattern.cs
@@ -1,8 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public abstract class Pattern : ScriptableObject
 {
-    public abstract void Execute(Actor target);
+    public abstract Task Execute(Actor target, CombatComponent combat);
+}
+
+public abstract class PatternBranch : ScriptableObject
+{
+    public abstract bool UseAlternative(Actor target, CombatComponent combat);
 }
